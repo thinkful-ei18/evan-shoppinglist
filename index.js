b528e4511467ccd1b836f121c0a968fba7e9349c
Evan Garrett <evang522@gmail.com>
2018-01-19 13:32:52 -0500

    Added "add-item" functionality.

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ const store = {
   searchTerm:false
 };
 
+let addItem = () => {
+  console.log('item has been queued');
+  let userInput = $('.js-shopping-list-entry').val();
+  $('.js-shopping-list-entry').val('');
+  store.items.push({name:userInput,checked:false});
+  renderList();
+};
 
 
 const handleNewItemAdded = () => {
 //1. Get input value user
 //2. Push user input to STORE.items
 //3. Regenerate html for Dom
-//4. Render Html to the DOM
+//4. Render Html to the DOM'
+  $('#js-shopping-list-form').on('submit',(event) => {
+
+    event.preventDefault();
+    addItem();
+  });
 };
+
 
 
 const handleItemChecked = () => {
@@ -31,22 +44,42 @@ const handleItemDeleted = () => {
 };
 
 
-const renderList = (arrayofItems) => {
-//1. declare a variable and set the value to an empty string.
+const createListString = (arrayOfItems) => {
+//1. declare a variable and set the value to an empty array.
 //2. Iterate over each member of the array, pushing an 'li' with traits
 // based on whether they are checked or not
-//3. return the initially declared variable 
-
+//3. JOIN and return the initially declared array, which now has all of the 'li's pushed to it
+  let storeLiArr = [];
+  arrayOfItems.forEach((item,index)=> {
+    storeLiArr.push(`
+      <li class='js-item-index-element' data-item-index="${index}">
+        <span class = "shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked': ''}">
+        ${item.name}
+        </span> 
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>
+    `);
+  });
+  // console.log(storeLiArr.join(''));
+  return storeLiArr.join('');
 };
 
-const pushToDom = (domData) => {
-//1 Push domData to the shopping LI container
-
+const renderList = () => {
+  const domData = createListString(store.items);
+  $('.js-shopping-list').html(domData);
 };
 
 
 const initiateQuiz = () => {
-
+  renderList();
+  handleNewItemAdded();
 };
 
 
